2e826f72645eea0e9e0f412ea0f8fcfa41141bc7
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-07-12 15:33:35 -0500

    enables unit tests for components using redux

--- src/components/__tests__/ExchangeRate.test.js
+++ src/components/__tests__/ExchangeRate.test.js
@@ -1,5 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../test-utils";
 import { ExchangeRate } from "../ExchangeRate";
+// import { Provider } from "react-redux";
+// import { store } from "../../store/store";
 
 test("renders title", () => {
   render(<ExchangeRate />);
